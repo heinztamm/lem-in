06c2384ed4264fb439de96513790462731b03d71
heinztamm <karl_tamm@hotmail.com>
2023-11-26 18:41:36 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# LEM-IN
+## Karl Heinrich Tamm / kartamm
+
+This project is a digital version of an ant farm. The ant farm composes of tunnels and rooms. Ants need to find their way from the starting room to the end-room turn-by-turn (moving rooms every turn), with the caveat that each room can only accommodate only one ant at a time.
+
+# Auditing
+
+Exercise description here: [description](https://01.kood.tech/git/root/public/src/branch/master/subjects/lem-in)
+
+Audit checklist here: [audit](https://01.kood.tech/git/root/public/src/branch/master/subjects/lem-in/audit)
+
+To make sure that an up-to-date version of the project is in use, run a "git pull" on the commandline
+
+Run the test script test.sh to run tests semi-automatically with examples from the audit resources
+```
+bash test.sh  
+or  
+./test.sh  
+if the above commands to not work, run  
+chmod +x test.sh  
+beforehand
+```
+or run commands manually  
+```
+go run . <path-to-file>
+
+as in:  
+  go run . example01.txt
+```
+
+### Thank you for taking the time to audit!!! Farewell! ###
---
+++ test.sh
@@ -0,0 +1,74 @@
+clear
+echo ---------------example00.txt-----------------
+go run . validExamples/example00.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example01.txt-----------------
+go run . validExamples/example01.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example02.txt-----------------
+go run . validExamples/example02.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example03.txt-----------------
+go run . validExamples/example03.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example04.txt-----------------
+go run . validExamples/example04.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example05.txt-----------------
+go run . validExamples/example05.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------badexample00.txt-----------------
+go run . validExamples/badexample00.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------badexample01.txt-----------------
+go run . validExamples/badexample01.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example06.txt-----------------
+go run . validExamples/example06.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------example07.txt-----------------
+go run . validExamples/example07.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------badexample00.txt-----------------
+go run . badExamples/badexample00.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo ---------------badexample01.txt-----------------
+go run . badExamples/badexample01.txt
+echo PRESS ANY KEY TO CONTINUE or ctrl-c to exit
+read -n 1 -s
+
+clear
+echo TESTS FINISHED
