f4800b6d2d58e74deab3a89708d3e8de7f0e39f8
heinztamm <karl_tamm@hotmail.com>
2023-11-26 18:56:42 +0200

    small changes to misinput handling

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"01.kood.tech/git/kartamm/lem-in/playground"
 )
 
 func main() {
-
+	if len(os.Args) != 2 {
+		fmt.Println("Error: wrong command format. Expected format: 'go run . <path-to-textfile-name>'")
+		os.Exit(1)
+	}
 	examplefilepath := os.Args[1]
 	antFarm := &playground.AntFarm{}
 	playground.FetchParams(examplefilepath, antFarm)
